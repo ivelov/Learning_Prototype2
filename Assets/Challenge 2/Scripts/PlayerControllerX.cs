623eb12ab0791015fae9fb5d5680bdadbe6e2b31
ivelov <ivelov.vlad1@gmail.com>
2022-02-02 17:25:05 +0200

    Challenge 2 end

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timeDelay = 2;
+    private float timePassed;
+
+    private void Start()
+    {
+        timePassed = timeDelay;
+    }
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timePassed >= timeDelay)
+        {
+            timePassed = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
